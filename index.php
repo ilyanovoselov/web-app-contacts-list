1148f2f86132b59755b4477d868e8db4be59c08b
ilyanovoselov <44347925+ilyanovoselov@users.noreply.github.com>
2018-10-22 04:53:39 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<title> Contacts book</title>	
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="/contacts.css"></link>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+		<link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+	</head>
+	<body>
+		
+		<div class="wrapper">
+			<div class="titlefield"> Contacts list </div>
+		
+			<i  class="material-icons loader"> autorenew </i>
+			<div id="print" class="book_roll">
+				<!-- cells appending here by jQuery function getJSON-->
+			</div>
+			<div class="textfield" id="print1"></div>
+		</div>
+
+	
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	<script type="text/javascript" src="/contacts.js"></script>
+
+	</body>
+</html>	
